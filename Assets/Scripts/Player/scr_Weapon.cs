40252382321c0449e0c77eac1355778c4f0f4f22
Pavel Bu <puchik006@mail.ru>
2024-07-13 03:29:45 +0300

    Gun begin

---
+++ Assets/Scripts/Player/scr_Weapon.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class scr_Weapon: MonoBehaviour
+{
+    [SerializeField] private scr_Weapon_Data _data;
+    [SerializeField] private Transform _firePoint;
+}
---
+++ Assets/Scripts/Player/scr_Weapon_Data.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class scr_Weapon_Data : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _picture;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _bulletSpeed = 10f;
+    [SerializeField] private float _fireRate = 1f;
+
+    public string Name => _name;
+    public GameObject Bullet => _bullet;
+    public float BulletSpeed => _bulletSpeed;
+    public float FireRate => _fireRate;
+}
